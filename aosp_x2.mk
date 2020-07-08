5030b3723f34e3d6aeb2581529a128e0c5f3df1f
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-07-08 16:57:28 +0530

    x2: Ship google recorder
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>

--- aosp_x2.mk
+++ aosp_x2.mk
@@ -11,6 +11,7 @@ CUSTOM_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
