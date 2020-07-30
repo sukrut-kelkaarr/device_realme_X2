celtare21/android_device_realme_x2

x2: rootdir: Import task cpu affinity script from RenderBroken
Signed-off-by: celtare21 <celtare21@gmail.com>

rootdir/Android.mk
@@ -15,3 +15,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.affinity.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.affinity.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)

sukrut-kelkaarr/device_realme_X2

Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>

rootdir/Android.mk
@@ -1,6 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
 # Common config scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.affinity.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.affinity.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.class_late.sh
 LOCAL_MODULE_TAGS  := optional