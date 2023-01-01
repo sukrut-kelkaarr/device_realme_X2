1923d629e10b3d549da546b97cdc41fbaafeb4e9
Sukrut4778 <sukrut4778@gmail.com>
2023-01-01 20:55:24 +0530

    X2: Switch to AIDL Pixel PowerHAL
    * Import PowerHal from https://android.googlesource.com/platform/hardware/google/pixel/+/refs/tags/android-13.0.0_r18/power-libperfmgr/
    * @jesec remove Google-specific VR and camera hints
    * @cyanogen Add support for tap-to-wake feature control
    * also build android.hardware.power.stats@1.0-service.mock
    * label sepolicies
    
    Signed-off-by: Sukrut4778 <sukrut4778@gmail.com>

--- device.mk
+++ device.mk
@@ -300,7 +300,11 @@ PPRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor \
-    android.hardware.power-service-qti
+    android.hardware.power-service.x2-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QMI
 PRODUCT_PACKAGES += \
@@ -363,7 +367,9 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    vendor/nxp/opensource/sn100x
+    vendor/nxp/opensource/sn100x \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Telephony
 PRODUCT_PACKAGES += \
